688941074641f16f1724e4f7ca52e68cc7028911
EmilyPlatzer <emilyplatzer@gmail.com>
2014-03-21 09:22:21 -0600

    prime detector now detects if 9 is not prime

--- lib/prime_detector.rb
+++ lib/prime_detector.rb
@@ -1,6 +1,7 @@
+require 'prime'
+
 class PrimeDetector
   def prime_number?(number)
-    true if number == 3
+    Prime.prime?(number)
   end
-
 end
--- spec/prime_detector_spec.rb
+++ spec/prime_detector_spec.rb
@@ -5,5 +5,8 @@ describe PrimeDetector do
     primey = PrimeDetector.new
     expect(primey.prime_number?(3)).to eq true
   end
-
+  it "detects that 9 is not prime" do
+    primey = PrimeDetector.new
+    expect(primey.prime_number?(9)).to eq false
+  end
 end
